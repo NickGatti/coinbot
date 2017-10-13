fc6f1bf12ee86dbe5e70062461076fe449f1574c
NickGatti <nick.gatti@gmail.com>
2017-10-12 20:33:20 -0700

    troubleshooting issues with sell orders

--- src/index.js
+++ src/index.js
@@ -294,16 +294,13 @@ function catchWebSocketMessage(data) {
     /*
         A valid order has been received and is now active.
         This message is emitted for every single valid order as soon as the matching engine receives it whether it fills immediately or not.
-
         The received message does not indicate a resting order on the order book.
         It simply indicates a new incoming order which as been accepted by the matching engine for processing.
         Received orders may cause match message to follow if they are able to begin being filled (taker behavior).
         Self-trade prevention may also trigger change messages to follow if the order size needs to be adjusted.
         Orders which are not fully filled or canceled due to self-trade prevention result in an open message and become resting orders on the order book.
-
         Market orders (indicated by the order_type field) may have an optional funds field which indicates how much quote currency will be used to buy or sell.
         For example, a funds field of 100.00 for the BTC-USD product would indicate a purchase of up to 100.00 USD worth of bitcoin.
-
         {
             "type": "received",
             "time": "2014-11-07T08:19:27.028459Z",
@@ -323,10 +320,8 @@ function catchWebSocketMessage(data) {
             This is the result of self-trade prevention adjusting the order size or available funds.
             Orders can only decrease in size or funds. change messages are sent anytime an order changes in size; this includes resting orders (open) as well as received but not yet open.
             Change messages are also sent when a new market order goes through self trade prevention and the funds for the market order have changed.
-
              Change messages for received but not yet open orders can be ignored when building a real-time order book.
              The side field of a change message and price can be used as indicators for whether the change message is relevant if building from a level 2 book.
-
             Any change message where the price is null indicates that the change message is for a market order.
             Change messages for limit orders will always have a price specified.
             {
@@ -738,7 +733,7 @@ function sellGapInfo() {
   let sellCount = 0;
   let sellTotal = 0;
   for (let i = 0; i < orderBook['sell'].length && myOrders.sell[myOrderIterator]; i++) {
-    if (orderBook['sell'][i].price >= (findHighestBuyPrice().price / filterBuyOrder().price).price) {
+    if (orderBook['sell'][i].price >= myOrders.sell[myOrderIterator].price) {
       break;
     } else {
       sellCount++;
@@ -861,14 +856,8 @@ function checkVars() {
   if (Number(findHighestBuyPrice().price)) findHighestBuyPrice().price = parseFloat(findHighestBuyPrice().price);
   if (Number(findLowestSellPrice().price)) findLowestSellPrice().price = parseFloat(findLowestSellPrice().price);
 
-  if (!myOrders.buy[myOrderIterator]) {
-    placeBuy();
-    myOrders.buy[myOrderIterator] = myOrders.buy[myOrderIterator];
-  }
-  if (!myOrders.sell[myOrderIterator]) {
-    placeSell();
-    myOrders.sell[myOrderIterator] = myOrders.sell[myOrderIterator];
-  }
+  if (!myOrders.buy[myOrderIterator]) placeBuy();
+  if (!myOrders.sell[myOrderIterator]) placeSell();
 
   if (myOrders.buy[myOrderIterator].price) {
     if (Number(myOrders.buy[myOrderIterator].price)) myOrders.buy[myOrderIterator].price = parseFloat(myOrders.buy[myOrderIterator].price);
@@ -995,7 +984,7 @@ app.get('/api', function(req, res) {
   });
 });
 
-app.listen(process.env.PORT, process.env.IP)  //(3000); //; //3000 Normal //process.env.PORT, process.env.IP for C9.io
+app.listen(3000); //(process.env.PORT, process.env.IP)  ; //3000 Normal //process.env.PORT, process.env.IP for C9.io
 //=============================================
 //=============================================
 //=============================================
